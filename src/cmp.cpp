4d47cb18e78b7f9475035c7c6a4b83ad78464a72
vicente <vicente.costa@ua.pt>
2023-03-04 11:35:29 +0000

    two examples and cmp print all sequences and where occur

--- src/cmp.cpp
+++ src/cmp.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <unistd.h>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+
 using namespace std;
 
 int main(int argc, char** argv) {
@@ -34,5 +39,69 @@ int main(int argc, char** argv) {
             }
         }
     }
+
+
+    map<string, vector<int> > dict;
+
+    // Create a text string, which is used to output the text file
+    char byte = 0;
+
+    string sequence = "";
+
+
+    // Create and open a text file
+    // cout << k << "\n";
+    ifstream input_file("./example/example2.txt");
+
+    int i =0;
+    // Use a while loop together with the getline() function to read the file line by line
+    while (input_file.get(byte)) {
+        // Output the text from the file
+        sequence+=byte;
+
+
+        if (sequence.length()==k){
+            if (dict.count(sequence) == 0){
+                dict.insert( make_pair(sequence, vector<int>()) );
+            }
+            dict[sequence].push_back(i+1);
+            // cout << i+k << "\n";
+            // cout << sequence << "\n";
+            sequence.erase(0, 1);
+
+        }
+
+        i++;
+        // // Traverse the string
+        // for (int i = 0; i < myText.length()-k+1; i++) {
+    
+        //     // Print current character
+        //     string sequence = myText.substr(i,k);
+        //     // cout << sequence << "\n";
+        //     if (dict.count(sequence) == 0){
+        //         dict.insert( make_pair(sequence, vector<int>(i)) );
+        //     }
+        //     dict[sequence].push_back(i+k);
+        //     // cout << i+k << "\n";
+
+        // }
+    }
+
+    for(map<string, vector<int> >::const_iterator it = dict.begin();
+    it != dict.end(); ++it)
+    {
+        std::cout << it->first << " : " ;
+
+        for (int index: it->second){
+            if (index != 0)
+                std::cout  << index << " " ;
+
+         }
+
+        std::cout << "\n";
+    }
+
+    // Close the file
+    input_file.close(); 
 
 }
