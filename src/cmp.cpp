629640c604c63a00a1cd176f28e24aac2fce9e40
JoaoBorgesUA <borgesjps@ua.pt>
2023-03-10 10:36:52 +0000

    still with many errors

--- src/cmp.cpp
+++ src/cmp.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <unistd.h>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <queue>
 using namespace std;
 
 int main(int argc, char** argv) {
@@ -34,5 +39,41 @@ int main(int argc, char** argv) {
             }
         }
     }
+
+    std::ifstream file("../example/file.txt");  // Open file for reading
+    std::map<string, std::vector<int>> dict;  // Create dictionary
+
+    if (file.is_open()) {  // Check if file was opened successfully
+        int position = 0;
+        std::string mystring;
+        file >> mystring;
+
+        //queue where puts k elements and "screenshots" the queue to a hashmap
+        queue<char> symbol;
+        
+
+        while (position < ) {  // Read file character by character
+            dict[symbol[position:position+k]].push_back(position);  // Add position to symbol's list
+            position++;
+        }
+
+        // Print dictionary
+        for (auto it = dict.begin(); it != dict.end(); ++it) {
+            std::cout << it->first << ": ";
+            for (auto pos : it->second) {
+                std::cout << pos << " ";
+            }
+            std::cout << std::endl;
+        }
+    } else {
+        std::cerr << "Error opening file" << std::endl;
+        return 1;
+    }
+
+    return 0;
+
+
+
+
 
 }
