992499d17574c8ef1f3d22a2185cb9b752302626
vicente <vicente.costa@ua.pt>
2023-03-17 14:29:10 +0000

    start cpm gen

--- src/cpm_gen.cpp
+++ src/cpm_gen.cpp
@@ -11,20 +11,27 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
-    map<string,  float > dict;
+    map<string,  map<string,  float > > dict;
     int c;
     int n = 100;                          // number of generated symbols
-    int k;
-    string filename="";
+    int k = 4;
 
-    while ((c = getopt(argc, argv, "n:o:")) != -1)
+    while ((c = getopt(argc, argv, "n:k:")) != -1)
     {
         switch (c)
         {
-            case 'o':
+            case 'k':
             {
-                filename = optarg;
-                std::cout << "o: " <<  optarg << "\n";
+                try
+                {
+                    k = stoi(optarg);
+                }
+                catch (exception &err)
+                {
+                    std::cout << "Invalid k argument" << std::endl;
+                    return EXIT_FAILURE;
+                }
+                std::cout << "K: " << k << std::endl;
                 break;
             }
             case 'n':
@@ -50,115 +57,111 @@ int main(int argc, char** argv) {
             }
         }
     }
+    string filename =  argv[optind];
     if (filename==""){
         std::cout << "Invalid output file path argument" << std::endl;
         return 1;
     }
-    ifstream cpm_output(filename);
+    ifstream example(filename);
+    char byte = 0;
 
-    // map<string, map<string, float > > dict;
+    string sequence = "";
+    
+    int i =0;
+    while (example.get(byte)) {
+        // Output the text from the file
+        string nextChar = "";
+        nextChar+= byte;
 
-    // Create a text string, which is used to output the text file
+        if (sequence.length()==k){
+            if (dict.count(sequence) == 0){
+                dict.insert( make_pair(sequence, map<string,  float >()) );
+                dict[sequence].insert( make_pair("total", 0 ));
+            }
+            if (dict[sequence].count(nextChar) == 0){
+                dict[sequence].insert(make_pair(nextChar, 0));
+            }
+            dict[sequence][nextChar]++;
+            dict[sequence]["total"]++;
 
-    // const char * filename = argv[optind];
-    // char k; 
+            sequence.erase(0, 1);
 
-    // Create and open a text file
-    // FILE *example = fopen(filename,"r");
+        }
 
-    // if(example== NULL){
-    //     std::cout << "Invalid example file path argument" << std::endl;
-    //     return 1;
-    // }
+        sequence+=nextChar;
+        i++;
+    }
+    std::cout << "Probs" << std::endl;
+
+    for(std::map<string,map<string,float>>::iterator it = dict.begin(); it != dict.end(); ++it) {
+        map<string,float> map2 = it->second;
+        string key = it->first; 
+        for(std::map<string,float>::iterator it2 =map2.begin(); it2 != map2.end(); ++it) {
+            dict[key][it2->first] = (it2->second) / dict[key]["total"];
+        }
+        dict[key].erase("total");
+    }
+    std::cout << "Print" << std::endl;
+
+    for(std::map<string,map<string,float>>::iterator it = dict.begin(); it != dict.end(); ++it) {
+        map<string,float> map2 = it->second;
+        string key = it->first; 
+        std::cout << key <<  ":"  << std::endl; 
+        for(std::map<string,float>::iterator it2 =map2.begin(); it2 != map2.end(); ++it) {
+            std::cout << "  -> "<< it2->second << std::endl; 
+
+        }
+    }
+    std::cout << "End" << std::endl;
 
 
     // Read data cpm output file and get dictionary with the sequence -> most likely symbol
-    string line;
-    while (getline(cpm_output, line)) { 
-        // Print the data of the string.
-        size_t pos = 0;
-        string sequence;
-        string simbol;
-        string delimiter = ":";
-        while ((pos = line.find(":")) != std::string::npos) {
-            sequence = line.substr(0, pos);
-            line.erase(0, pos + delimiter.length()+1);
-            pos = 0;
-            k = sequence.length();
-            break;
-        }
-        std::cout << sequence << ":"<< line << "|" << std::endl;
+    // string line;
+    // while (getline(example  , line)) { 
+    //     // Print the data of the string.
+    //     size_t pos = 0;
+    //     string sequence;
+    //     string simbol;
+    //     string delimiter = ":";
+    //     while ((pos = line.find(":")) != std::string::npos) {
+    //         sequence = line.substr(0, pos);
+    //         line.erase(0, pos + delimiter.length()+1);
+    //         pos = 0;
+    //         k = sequence.length();
+    //         break;
+    //     }
+    //     std::cout << sequence << ":"<< line << "|" << std::endl;
 
-        dict.insert( make_pair(sequence, stof(line)));  
-        // delimiter = "-";
-
-        // dict.insert( make_pair(sequence, map<string, float >()) );
-        // std::cout << line << std::endl;
-        // while ((pos = line.find("-")) != std::string::npos) {
-        //     simbol = line.substr(0, pos);
-        //     std::cout << simbol << std::endl;
-        //     line.erase(0, pos + delimiter.length());
-        //     pos = 0;
-        //     int pos2=0;
-        //     delimiter = ",";
-        //     while ((pos2 = line.find(",")) != std::string::npos) {
-        //         string prob = line.substr(0, pos2);
-        //         dict[sequence].insert( make_pair(simbol, stof(prob)));  
-        //         pos2=0;
-        //         break;
-        //     }
-        // }
-
-    }
-    
-    // Close the file object.
-    cpm_output.close(); 
-
-    
-    // for(map<string, map<string, float > >::const_iterator it = dict.begin();
-    // it != dict.end(); ++it)
-    // {
-    //     std::cout << it->first << " : "  << it->second << "\n";
+    //     dict.insert( make_pair(sequence, stof(line)));  
 
     // }
-    std::cout << "end File chapter 1"<< std::endl;
-    // char ch;
-    // string sequence = "";
-    // fseek(example, 0, SEEK_END);
-    // long fileLength = ftell(example);
-    // fseek(example, (fileLength - k), SEEK_SET);
     
-    // std::cout << "end File chapter 2"<< std::endl;
-    // do {
-    //     ch = fgetc(example);
-    //     if (ch != EOF){
-    //         sequence += ch; 
-    //     }
-        
+    // Close the file object.
+    example.close(); 
 
-    // } while (ch != EOF);
+    
+
 
 
     // fclose(example);
-    string example = argv[optind];
     
-    srand( (unsigned)time( NULL ) );
-    for(int i=0; i<n;i++){
-        float total = 0.0;
-        float rand_val = (float) rand()/RAND_MAX;
-        std::cout << rand_val << std::endl;
-        for(std::map<string,float>::iterator it = dict.begin(); it != dict.end(); ++it) {
-            total += it-> second;
-            if(rand_val < total ){
-                example += it-> first;
-                break;
-            }
-        }
+    // srand( (unsigned)time( NULL ) );
+    // for(int i=0; i<n;i++){
+    //     float total = 0.0;
+    //     float rand_val = (float) rand()/RAND_MAX;
+    //     std::cout << rand_val << std::endl;
+    //     for(std::map<string,float>::iterator it = dict.begin(); it != dict.end(); ++it) {
+    //         total += it-> second;
+    //         if(rand_val < total ){
+    //             example += it-> first;
+    //             break;
+    //         }
+    //     }
 
-    }
+    // }
 
 
-    std::cout << "output: " << example<< std::endl;
+    // std::cout << "outp  ut: " << example << std::endl;
 
 
 
